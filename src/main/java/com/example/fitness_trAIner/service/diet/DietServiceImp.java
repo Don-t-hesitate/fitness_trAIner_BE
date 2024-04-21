59947b6a607f83143d174bf68353971698270d8a
LimpidusCoruscare <chelhwankim@gmail.com>
2024-04-22 01:49:09 +0900

    diet 기능 구현

--- src/main/java/com/example/fitness_trAIner/service/diet/DietServiceImp.java
+++ src/main/java/com/example/fitness_trAIner/service/diet/DietServiceImp.java
@@ -22,11 +22,11 @@ public class DietServiceImp implements DietService{
 
         foodList = switch (category) {
             case "korean" -> Arrays.asList("흑미밥", "된장찌개", "제육볶음", "순살 후라이드 치킨", "김치");
-            case "chinese" -> Arrays.asList("흑미밥", "된장찌개", "제육볶음", "순살 후라이드 치킨", "김치");
-            case "japanese" -> Arrays.asList("흑미밥", "된장찌개", "제육볶음", "순살 후라이드 치킨", "김치");
-            case "western" -> Arrays.asList("흑미밥", "된장찌개", "제육볶음", "순살 후라이드 치킨", "김치");
-            case "dessert" -> Arrays.asList("흑미밥", "된장찌개", "제육볶음", "순살 후라이드 치킨", "김치");
-            case "fastfood" -> Arrays.asList("흑미밥", "된장찌개", "제육볶음", "순살 후라이드 치킨", "김치");
+            case "chinese" -> Arrays.asList("짜장면", "탕수육", "마라탕", "유산슬", "제로 탕후루");
+            case "japanese" -> Arrays.asList("스시", "우메보시", "타코야끼", "단무지", "사케");
+            case "western" -> Arrays.asList("라따뚜이", "블랙 푸딩", "알리오 올리오", "포케", "피시 앤 칩스");
+            case "fastfood" -> Arrays.asList("샌드위치", "햄버거", "라면", "떡볶이", "피자");
+            case "dessert" -> Arrays.asList("버블티", "치즈케이크", "마카롱", "티라미수", "와플");
             default -> foodList;
         };
 
