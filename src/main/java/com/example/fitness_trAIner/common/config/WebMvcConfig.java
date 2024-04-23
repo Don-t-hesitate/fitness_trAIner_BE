505b3bef07fa3ff432c520f2839250da195d82f3
LimpidusCoruscare <chelhwankim@gmail.com>
2024-04-24 00:12:08 +0900

    루트 URL 접근 시 Spring MVC로 리액트 정적 파일을 브라우저에 제공

--- src/main/java/com/example/fitness_trAIner/common/config/WebMvcConfig.java
+++ src/main/java/com/example/fitness_trAIner/common/config/WebMvcConfig.java
@@ -13,20 +13,27 @@ import java.util.List;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
+    // 정적 리소스 핸들러 설정
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // "/" 패턴의 요청을 "classpath:/static/build/" 위치에서 찾도록 설정
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/build/")
+                .addResourceLocations("classpath:/static/")
                 .setCachePeriod(0);
 
+        // "/static/js/**" 패턴의 요청을 "classpath:/static/build/static/js/" 위치에서 찾도록 설정
         registry.addResourceHandler("/static/js/**")
-                .addResourceLocations("classpath:/static/build/static/js/")
+                .addResourceLocations("classpath:/static/static/js/")
                 .setCachePeriod(0);
+
+        // "/static/css/**" 패턴의 요청을 "classpath:/static/build/static/css/" 위치에서 찾도록 설정
         registry.addResourceHandler("/static/css/**")
-                .addResourceLocations("classpath:/static/build/static/css/")
+                .addResourceLocations("classpath:/static/static/css/")
                 .setCachePeriod(0);
+
     }
+
+    // 커스텀 HttpMessageConverter 추가
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new CustomResourceHttpMessageConverter());
--- src/main/java/com/example/fitness_trAIner/controller/web/ReactController.java
+++ src/main/java/com/example/fitness_trAIner/controller/web/ReactController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReactController {
+
+    // ceprj.gachon.ac.kr:60008/ 로 접속하면 index.html(react 정적파일)을 반환
     @GetMapping({"/", "/{x:[\\w\\-]+}"})
     public String getIndex() {
         return "index.html";
