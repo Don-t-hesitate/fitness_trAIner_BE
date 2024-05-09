7d066a219c17d625bc253167f2e6afe58f102ea4
LimpidusCoruscare <chelhwankim@gmail.com>
2024-05-09 23:20:37 +0900

    사용자 식단 선호도 수정 URI의 불필요한 디렉토리 삭제

--- src/main/java/com/example/fitness_trAIner/controller/admin/AdminController.java
+++ src/main/java/com/example/fitness_trAIner/controller/admin/AdminController.java
@@ -111,7 +111,7 @@ public class AdminController {
                 .build();
     }
 
-    @PutMapping("/food/preferences/{id}")
+    @PutMapping("/food/preferences")
     @Operation(summary = "사용자 식단 선호도 수정", description = "사용자 식단 선호도 수정")
     @ApiResponse(responseCode = "200", description = "성공", useReturnTypeSchema = true)
     @ApiResponse(responseCode = "400", description = "에러 발생", content = @Content(schema = @Schema(implementation = GlobalExceptionResponse.class)))
